689eed2289cb386cdc7752cf601886f41fde2520
thoorranie <thoorranie1@gmail.com>
2026-08-15 00:58:10 +0900

    Build N-hangsi community MVP

---
+++ lib/api/client.ts
@@ -0,0 +1,1 @@
+export const API_BASE_URL=process.env.NEXT_PUBLIC_API_URL??"http://localhost:8080/api";export async function apiRequest<T>(path:string,init?:RequestInit):Promise<T>{const res=await fetch(API_BASE_URL+path,{...init,headers:{"Content-Type":"application/json",...init?.headers}});if(!res.ok)throw new Error("API "+res.status);return res.json() as Promise<T>}
